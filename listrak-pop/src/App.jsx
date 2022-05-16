0d477d9a484a73391fcf288bfef8971e9ba7eb26
Salar Malik <salarxmalik@gmail.com>
2022-05-15 21:16:27 -0700

    pop up skeleton implemented

--- listrak-pop/src/App.jsx
+++ listrak-pop/src/App.jsx
@@ -1,19 +1,17 @@
-import './App.css';
+import React, { useState } from 'react';
 import PopUp from './components/PopUp.jsx';
 import ConfirmationPage from './components/ConfirmationPage.jsx';
 
 
 function App() {
+  const [page, setPage] = useState(false);
   return (
     <div>
       <h1>
         Listrak Pop-Up
       </h1>
       <div>
-        <PopUp/>
-      </div>
-      <div>
-        <ConfirmationPage/>
+      {page ? <ConfirmationPage/> : <PopUp/>}
       </div>
     </div>
   )
--- listrak-pop/src/components/PopUp.jsx
+++ listrak-pop/src/components/PopUp.jsx
@@ -1,7 +1,35 @@
+import React, { useState } from 'react';
+
 function PopUp() {
   return (
-    <div>
-      Here's ya pop up
+    <div classname="popupbrowsercontatiner">
+      <h2>
+        Pop Up
+      </h2>
+      <div classname="styletextbrowser">
+        Get Your Style On
+      </div>
+      <div classname="andsignbrowser">
+        {"&"}
+      </div>
+      <div classname="numberbrowser">
+        20
+      </div>
+      <div classname="percentoffbrowser">
+        {"% OFF"}
+      </div>
+      <div classname="emailandsubmitbrowser">
+        <div classname="emailaddressformbrowser">
+        <form>
+          <label>
+            <input type="email" name="email" placeholder="Email Address" required/>
+          </label>
+        </form>
+        </div>
+        <div classname="submissionbuttonbrowser">
+          <button>Sign Up</button>
+        </div>
+      </div>
     </div>
   )
 }
